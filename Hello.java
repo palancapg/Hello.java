37652cb25bd9da958fa3055508b3edfbbf761cc8
palancapg <54410880+palancapg@users.noreply.github.com>
2019-08-22 12:04:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/************************************
+ * Hello.java
+ *************************************
+ * Running Java Correctly
+ * __________________________________
+ * Phillip Palanca
+ * August 22 2019 
+ * CMSC 225 001 
+ ***********************************/
+
+ public class Hello {
+  public static void printHeading() {
+   System.out.println("Phillip Palanca");
+   System.out.println("Hello.java");
+   System.out.println("Running Java Correctly");
+   System.out.println("August 22 2019");
+   System.out.println("CMSC 225 001");
+   System.out.println();
+  }
+
+  public static void main(String [] args) {
+ 	 printHeading();
+ 	 System.out.println("Hello");
+  }
+ }
+
